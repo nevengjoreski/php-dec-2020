33ae948ebf0247ab01be7bd3ebdf4c6ba95799ab
Neven.Gjoreski <n.gjoreski@blexr.com>
2020-12-24 20:12:34 +0100

    c4 temp

--- c3/functions.php
+++ c3/functions.php
@@ -45,3 +45,56 @@ function getSumOfNumbers( $b1, $b2, $b3 = 0 ){
 
 // todo: global variables
 // todo: nizata so min max vo funkcija
+echo '<br>';
+$niza = [ 12 , 45, 65, 87, 72, 3, 99, 54, -1 ];
+
+$presmetana = presmetkiOdNiza($niza);
+
+$niza2 = [ 12 , 45];
+$presmetana2 = presmetkiOdNiza($niza2);
+
+echo "  Minimumot e : {$presmetana['min']} <br> 
+        Maksimumot e : {$presmetana['max']} <br> 
+        Srednata vrednost : {$presmetana['sredna']} <br> 
+        Proizvodot e : {$presmetana['proizvod']} <br>";
+
+echo "  Minimumot e : {$presmetana2['min']} <br> 
+    Maksimumot e : {$presmetana2['max']} <br> 
+    Srednata vrednost : {$presmetana2['sredna']} <br> 
+    Proizvodot e : {$presmetana2['proizvod']} <br>";
+
+
+function presmetkiOdNiza($niza){
+    // global $niza;
+        
+    $min = $niza[0];
+    $max = $niza[0];
+    
+    $broj_na_elementi = count($niza);
+    $zbir = 0;
+    $proizvod = 1;
+    
+    foreach( $niza as $element){
+    
+        if( $min > $element ){
+            $min = $element;
+        }
+    
+        if( $max < $element ){
+            $max = $element;
+        }
+    
+        $zbir += $element;
+        $proizvod *= $element;
+    }
+    
+    $sredna = $zbir / $broj_na_elementi;
+    $sredna = round($sredna , 2);
+
+    return [ 
+        'min'       => $min ,
+        'max'       => $max, 
+        'sredna'    => $sredna,
+        'proizvod'  => $proizvod
+    ];
+}
